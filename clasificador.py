f127a7440fe172a72ca3fec8e31227c79fe03989
AlanAlbertMn <A01328928@itesm.mx>
2021-11-07 19:02:49 -0600

    Diccionario global de palabras con apariciones positivas y negativas

--- clasificador.py
+++ clasificador.py
@@ -5,6 +5,18 @@ import codecs
 
 filename = 'yelp_labelled.txt'  
 
+def mergeDict(dict1, dict2):
+    # Merge dictionaries and keep values of common keys in list
+    dict3 = {**dict1, **dict2}
+    for key, value in dict3.items():
+       if key in dict1 and key in dict2:
+               dict3[key] = [value[0] , dict1[key][1]]
+    return dict3
+    # Merge dictionaries and add values of common keys in a list
+    dict3 = mergeDict(dict1, dict2)
+    print('Dictionary 3 :')
+    print(dict3)
+
 def countWords(str):
     occurrencesPos = dict()
     occurrencesNeg = dict()
@@ -15,21 +27,37 @@ def countWords(str):
             words = sentence.split()
             for word in words:
                 if word in occurrencesPos:
-                    occurrencesPos[word] += 1
+                    tmp = occurrencesPos[word]
+                    tmp[0]+=1
+                    occurrencesPos[word] = tmp
                 else:
-                    occurrencesPos[word] = 1
+                    occurrencesPos[word] = [1, 0]
         else:
             words = sentence.split()
             for word in words:
                 if word in occurrencesNeg:
-                    occurrencesNeg[word] += 1
+                    tmp = occurrencesNeg[word]
+                    tmp[1]+=1
                 else:
-                    occurrencesNeg[word] = 1
+                    occurrencesNeg[word] = [0, 1]
+    positivas = occurrencesPos.get('1')[0]
+    negativas = occurrencesNeg.get('0')[1]
     occurrencesPos.pop('1')
     occurrencesNeg.pop('0')
+    # print("Palabras positivas: {}" .format(sum(occurrencesPos.values())))
+    # print("Palabras negativas: {}" .format(sum(occurrencesNeg.values())))
+    print("Oraciones Positivas: {}" .format(positivas))
+    print("Oraciones Negativas: {}" .format(negativas))
+    vocabulario = {**occurrencesPos , **occurrencesNeg}
+    print("Vocabulario: {}" .format(len(vocabulario)))
+
     print("Positivas: {}" .format(occurrencesPos))
-    print("\n")
-    print("Negativas: {}" .format(occurrencesNeg))
+    print("\nNegativas: {}" .format(occurrencesNeg))
+
+    words_table = mergeDict(occurrencesNeg, occurrencesPos)
+    print("")
+    print(words_table)
+
 
 try:
     with open(filename,'r',encoding="utf-8") as f:
