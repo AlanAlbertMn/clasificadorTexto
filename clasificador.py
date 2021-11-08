9a65cd3fbf302bbe98ff83ca0804006fa668ca6f
AlanAlbertMn <A01328928@itesm.mx>
2021-11-08 01:32:06 -0600

    Palabras +-, oraciones +-, logaritmos y diccionario global

--- clasificador.py
+++ clasificador.py
@@ -2,8 +2,16 @@
 import string
 import sys
 import codecs
+import math
 
-filename = 'yelp_labelled.txt'  
+filename = 'yelp_labelled.txt'
+
+def bayes(dictPos, dictNeg, dictTotal, pos, neg):
+    dictLogs = dict()
+    for i, j in dictTotal.items():
+        tmpPos = ((dictTotal[i][0]+1)/(pos+len(dictTotal)))
+        tmpNeg = ((dictTotal[i][1]+1)/(neg+len(dictTotal)))
+        print("{} {} {} {} {}" .format(i, dictTotal[i][0], dictTotal[i][1], math.log10(tmpPos), math.log10(tmpNeg)))
 
 def mergeDict(dict1, dict2):
     # Merge dictionaries and keep values of common keys in list
@@ -12,19 +20,15 @@ def mergeDict(dict1, dict2):
        if key in dict1 and key in dict2:
                dict3[key] = [value[0] , dict1[key][1]]
     return dict3
-    # Merge dictionaries and add values of common keys in a list
-    dict3 = mergeDict(dict1, dict2)
-    print('Dictionary 3 :')
-    print(dict3)
 
 def countWords(str):
     occurrencesPos = dict()
     occurrencesNeg = dict()
-    sentences = str.split('\n')
+    sentences = str.split('\n') #separar por oraciones
     palabrasPos = 0
     palabrasNeg = 0
     for sentence in sentences:
-        if sentence.endswith("1"):
+        if sentence.endswith("1"): #si la oracion antes de separarla termina con 1 significa que es positiva
             words = sentence.split()
             for word in words:
                 palabrasPos+=1
@@ -34,7 +38,7 @@ def countWords(str):
                     occurrencesPos[word] = tmp
                 else:
                     occurrencesPos[word] = [1, 0]
-        else:
+        else: #si la oracion antes de separarla termina con 0 significa que es negativa
             words = sentence.split()
             for word in words:
                 palabrasNeg+=1
@@ -50,20 +54,20 @@ def countWords(str):
     total = positivas + negativas
     occurrencesPos.pop('1')
     occurrencesNeg.pop('0')
-    print("Palabras positivas: {}" .format(palabrasPos))
-    print("Palabras negativas: {}" .format(palabrasNeg))
-    print("Oraciones Positivas: {}" .format(positivas/total))
-    print("Oraciones Negativas: {}" .format(negativas/total))
+    # print("Palabras positivas: {}" .format(palabrasPos))
+    # print("Palabras negativas: {}" .format(palabrasNeg))
+    # print("Oraciones Positivas: {}" .format(positivas/total))
+    # print("Oraciones Negativas: {}" .format(negativas/total))
     vocabulario = {**occurrencesPos , **occurrencesNeg}
-    print("Vocabulario: {}" .format(len(vocabulario)))
+    # print("Vocabulario: {}" .format(len(vocabulario)))
 
-    print("Positivas: {}" .format(occurrencesPos))
-    print("\nNegativas: {}" .format(occurrencesNeg))
+    # # print("Positivas: {}" .format(occurrencesPos))
+    # # print("\nNegativas: {}" .format(occurrencesNeg))
 
     words_table = mergeDict(occurrencesNeg, occurrencesPos)
-    print("")
-    print(words_table)
-
+    # print("")
+    # print(len(words_table))
+    bayes(occurrencesPos, occurrencesNeg, words_table, palabrasPos, palabrasNeg)
 
 try:
     with open(filename,'r',encoding="utf-8") as f:
